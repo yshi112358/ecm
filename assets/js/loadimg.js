16955a729623a33e135e0693bbd12307bf4d7a4b
yshi112358 <yshi112358@gmail.com>
2022-11-14 16:53:27 +0900

    No Imageの追加

--- assets/js/loadimg.js
+++ assets/js/loadimg.js
@@ -9,9 +9,13 @@ var article = [];
 d3.csv("https://docs.google.com/spreadsheets/d/1bntxqXyM38BB3iKHJ035wKEBI1gGYDBDCZp-KBok9jM/export?format=csv&range=A3:D", function (error, data) {
     for (let i = data.length - 1; i >= 0; i--) {
         article[i] = new Article(data[i].Title, data[i].Content, data[i].Img, data[i].Timestamp);
+        console.log(article[i].img == '');
         $('.recent-page').append(
             "<button class='article' id='article" + i + "'>" +
-            "<img src=" + article[i].img.replace("https://drive.google.com/open?id=", "https://drive.google.com/uc?id=") + " class='thumbnail'>" +
+            ((article[i].img != '')
+                ? ("<img src='" + article[i].img.replace("https://drive.google.com/open?id=", "https://drive.google.com/uc?id=") + "' class='thumbnail'>")
+                : ("<img src='./assets/img/NoImage.png' class='thumbnail'>")
+            ) +
             "<div class='title'>" + article[i].title + "</div>" +
             "<div class='content'>" + article[i].content + "</div>" +
             "</button>");
@@ -27,5 +31,9 @@ d3.csv("https://docs.google.com/spreadsheets/d/1bntxqXyM38BB3iKHJ035wKEBI1gGYDBD
 function loadArticle(data) {
     $('.article-detail .article-title').html(data.title);
     $('.article-detail .article-content').html(data.content);
-    $('.article-detail .article-thumbnail').attr('src', data.img.replace("https://drive.google.com/open?id=", "https://drive.google.com/uc?id="));
+    $('.article-detail .article-thumbnail').attr('src', (
+        (data.img != '')
+            ? (data.img.replace("https://drive.google.com/open?id=", "https://drive.google.com/uc?id="))
+            : './assets/img/NoImage.png'
+    ));
 }
