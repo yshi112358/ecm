f3830b6af56da6f7905b72e74f5ccce5a46b745d
yshi112358 <yshi112358@gmail.com>
2022-11-14 18:00:53 +0900

    add animation

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,3 +1,10 @@
 $(".article-bg").on('click', function () {
     $('.article-bg').fadeOut();
 });
+$('.top-right').fadeIn().queue(function () {
+    $('.top-left').fadeIn().queue(function () {
+        $('.top-slogan-box').fadeIn();
+        $('.top-slogan-box').css('display', 'flex');
+    });
+});
+
